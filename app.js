2bdcb9ddd6cfdbd897da266c859d104fd21caf98
Abdulkafi88 <123754218+Abdulkafi88@users.noreply.github.com>
2023-09-05 12:52:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,58 +18,57 @@ const audio = document.getElementById("song")
 const start = document.querySelector("#play i")
 const end = document.getElementById("end")
 const img = document.getElementById("thumb")
-const range = document.getElementById("progress")
-const next = document.querySelector(".next i")
-const previuse = document.querySelector(".back i ")
-const title = document.getElementById('title')
-const musician = document.getElementById('musician')
+const progress = document.getElementById("progress")
+const next = document.querySelector(".next ")
+const previuse = document.querySelector(".back ")
+const title = document.getElementById("title")
+const musician = document.getElementById("musician")
 
-// window.addEventListener('DOMContentLoaded', frisUser)
-start.addEventListener("click",()=>{
-    frisUser()
-} )
+window.addEventListener("DOMContentLoaded", frisUser)
 
-    next.addEventListener("click", (e) => {
-     currentSongIndex = (currentSongIndex + 1) % songs.length 
-     img.src = songs[currentSongIndex].img
-      title.textContent = songs[currentSongIndex].name
-        musician.textContent = songs[currentSongIndex].artists
-     audio.src = songs[currentSongIndex].link
+start.addEventListener("click", () => {
+  frisUser()
+})
 
-      if (start.classList.contains("bx-pause")) {
-        audio.play()
-      }
-    }) 
+next.addEventListener("click", (e) => {
+  currentSongIndex = (currentSongIndex + 1) % songs.length
+  img.src = songs[currentSongIndex].img
+  title.textContent = songs[currentSongIndex].name
+  musician.textContent = songs[currentSongIndex].artists
+  audio.src = songs[currentSongIndex].link
 
-    previuse.addEventListener('click', ()=>{
-      currentSongIndex = (currentSongIndex - 1) % songs.length
-      img.src = songs[currentSongIndex].img
-      title.textContent = songs[currentSongIndex].name
-      musician.textContent = songs[currentSongIndex].artists
-      audio.src = songs[currentSongIndex].link
+  if (start.classList.contains("bx-pause")) {
+    audio.play()
+  }
+})
 
-      if (start.classList.contains("bx-pause")) {
-        audio.play()
-      }
-      
-    })
-function frisUser(){
-    if (start.classList.contains("bx-pause")) {
-      audio.pause()
-      start.classList.remove("bx-pause")
-      start.classList.add("bx-play")
-      img.src = songs[currentSongIndex].img
-      title.textContent = songs[currentSongIndex].name
-        musician.textContent = songs[currentSongIndex].artists
-      img.classList.remove("play")
-    } else {
-      start.classList.remove("bx-play")
-      start.classList.add("bx-pause")
-       title.textContent = songs[currentSongIndex]. name
-       musician.textContent = songs[currentSongIndex].artists
-      img.src = songs[currentSongIndex].img
-      img.classList.add("play")
-      audio.play()
-    }
+previuse.addEventListener("click", () => {
+  currentSongIndex = (currentSongIndex - 1) % songs.length
+  img.src = songs[currentSongIndex].img
+  title.textContent = songs[currentSongIndex].name
+  musician.textContent = songs[currentSongIndex].artists
+  audio.src = songs[currentSongIndex].link
 
+  if (start.classList.contains("bx-pause")) {
+    audio.play()
+  }
+})
+function frisUser() {
+  if (start.classList.contains("bx-pause")) {
+    audio.pause()
+    start.classList.remove("bx-pause")
+    start.classList.add("bx-play")
+    img.src = songs[currentSongIndex].img
+    title.textContent = songs[currentSongIndex].name
+    musician.textContent = songs[currentSongIndex].artists
+    img.classList.remove("play")
+  } else {
+    start.classList.remove("bx-play")
+    start.classList.add("bx-pause")
+    title.textContent = songs[currentSongIndex].name
+    musician.textContent = songs[currentSongIndex].artists
+    img.src = songs[currentSongIndex].img
+    img.classList.add("play")
+    audio.play()
+  }
 }
